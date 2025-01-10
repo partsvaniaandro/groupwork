7627ae0680ac240fb48dc2d7f10b0b670d31742c
partsvaniaandro <ablabudaandro2@gmail.com>
2025-01-10 21:16:44 +0400

    gw

---
+++ 1Q/index.js
@@ -0,0 +1,14 @@
+const questions = [
+    {
+        question: "/1Q/1-question quize img/Q1.png",
+        
+        answers: [
+            {image: "/1Q/1-question quize img/Q1-01.png", correct: false},
+            {image: "/1Q/1-question quize img/Q1-02.png", correct: false},
+            {image: "/1Q/1-question quize img/Q1-03t.png", correct: true},
+            {image: "/1Q/1-question quize img/Q1-04.png", correct: false},
+            {image: "/1Q/1-question quize img/Q1-05.png", correct: false},
+            {image: "/1Q/1-question quize img/Q1-06.png", correct: false}
+        ]
+    }
+]
